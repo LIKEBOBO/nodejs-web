267a73af33e13b0e9159eb933ca7161928168763
김태형_201411517 <rlaxogud94@naver.com>
2020-02-15 16:01:20 +0900

    Update app.js

--- bobo#1/app.js
+++ bobo#1/app.js
@@ -2,131 +2,139 @@ const http=require('http');
 const express=require('express');
 const app=express();
 const router=express.Router();
-const path=require('path');
 const bodyParser=require('body-parser');
+const path=require('path');
+
+app.locals.pretty=true;
+
+const chart=['index','home','profile','car list'];
+let carlist=[
+    {_id:1,name:'Sonata',price:1000,company:'현대',year:2019},
+    {_id:2,name:'Avante',price:1000,company:'현대',year:2020},
+    {_id:3,name:'K9',price:10000,company:'KIA',year:2019},
+    {_id:4,name:'BMW',price:12000,company:'BMW',year:2018},
+    {_id:5,name:'BENZ',price:11000,company:'BENZ',year:2017}
+];
+
+let seq=5;
 
 
-// form에서 전달된 POST 데이터를 받기 위해서 설정!
+
+
+app.set('port',6123);
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// ejs는 레이아웃 잡는 데에 쓰는 것이 좋다!
-app.set('port',3000);
-app.set('views',path.join(__dirname,'views')); // 경로 접두어  path.join을 사용하면 운영체제에 상관없이 사용 가능!
-app.set('view engine','pug'); // 경로 접미어
-
 router.route('/').get((req,res)=>{
-   req.app.render('index',{name:'보보'},(err,html)=>{
-       if(err){
-           console.log(err);
-       }
-       res.end(html);
-
-   });
+    req.app.render('index',{name:'보보'},(err,html)=>{
+       if(err) throw err;
+       res.send(html);
+    });
 });
 
-let car_list=[
-    {_id:0,name:'소나타',price:1000,company:'현대',year:2019},
-    {_id:1,name:'K7',price:2000,company:'KIA',year:2020},
-    {_id:2,name:'SM6',price:3000,company:'SAMSUNG',year:2021},
-    {_id:3,name:'BMW',price:4000,company:'BMW',year:2022}
-];
 
-let seq=3;
-
+router.route('/home').get((req,res)=>{
+    req.app.render('home',{name:'보보'},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
+    });
+});
 
 router.route('/profile').get((req,res)=>{
-    req.app.render('profile',{cars:car_list},(err,html)=>{
-        if(err){
-            console.log(err);
-        }
-        res.end(html);
- 
+    req.app.render('profile',{name:'보보'},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
     });
- });
- 
-
- router.route('/car/list').get((req,res)=>{
-    req.app.render('car_list',{name:'행보보'},(err,html)=>{
-        if(err){
-            console.log(err);
-        }
-        res.end(html);
- 
-    });
- });
- 
-
-
- router.route('/home').get((req,res)=>{
-    req.app.render('home',{name:'행보보'},(err,html)=>{
-        if(err){
-            console.log(err);
-        }
-        res.end(html);
- 
-    });
- });
- 
-router.route('/car/input').get((req,res)=>{
-    req.app.render('car_form',{},(err,html)=>{
-        res.end(html);
-    });
-})
-
-router.route('/car/detail/:_id').get((req,res)=>{
-    let _id=parseInt(req.params._id);
-    console.log(_id);
-
-
-    let idx=carList.findIndex((item,index)=>{
-        if(item._id==_id) return index;
-    });
-
-    console.log("idx : ",idx);
-    if(idx!==-1){
-        req.app.render('car_detail',{car:carList[idex]},(err,html)=>{
-            res.end(html);
-
-        });
-    }
-    else{
-        res.end("Not Found Index");
-    }
-    
 });
 
-
-router.route('/car/modify').get((req,res)=>{
-    req.app.render('car_form',{},(err,html)=>{
-        res.end(html);
+router.route('/car/list').get((req,res)=>{
+    req.app.render('car_list',{name:'보보',cars:carlist},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
     });
-})
-
+});
 
 router.route('/car/input').get((req,res)=>{
-    req.app.render('car_form',{},(err,html)=>{
-        res.end(html);
+    req.app.render('car_form',{name:'보보'},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
     });
-})
-
+});
 
 router.route('/car/input').post((req,res)=>{
-    let cardata={
+    let car_data={
         _id:++seq,
         name:req.body.name,
         price:req.body.price,
         company:req.body.company,
         year:req.body.year
     };
+    carlist.push(car_data);
+    res.redirect('/car/list');
+});
 
-    car_list.push(cardata);
+router.route('/car/modify/:_id').get((req,res)=>{
+    let _id=parseInt(req.params._id);
+    req.app.render('car_modify',{name:'보보',car:carlist[_id-1]},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
+    });
+});
+
+router.route('/car/modify/:_id').post((req,res)=>{
+    let _id=parseInt(req.params._id);
+    carlist[_id-1].name=req.body.name;
+    carlist[_id-1].price=req.body.price;
+    carlist[_id-1].company=req.body.company;
+    carlist[_id-1].year=req.body.year;
+    res.redirect('/car/list');
+});
+
+router.route('/car/delete/:_id').get((req,res)=>{
+    let _id=parseInt(req.params._id)-1;
+    req.app.render('car_delete',{name:'보보',car:carlist[_id]},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
+    });
 });
 
 
+router.route('/car/delete/:_id').post((req,res)=>{
+    let _id=parseInt(req.params._id)-1;
+    carlist.splice(_id,1);
+    res.redirect('/car/list');
+});
+
+router.route('/car/detail/:_id').get((req,res)=>{
+    let _id=parseInt(req.params._id);
+    let idx=carlist.findIndex((item,index)=>{
+        return item._id==_id;
+    });
+    if(idx!=-1){
+        req.app.render('car_detail',{name:'보보',car:carlist[idx]},(err,html)=>{
+            if(err) throw err;
+            res.send(html);
+        });
+    }
+    else{
+        res.send("not found!!!");
+    }
+});
+
+router.route('/profile').get((req,res)=>{
+    req.app.render('profile',{name:'보보'},(err,html)=>{
+        if(err) throw err;
+        res.send(html);
+    });
+});
+
 
 app.use('/',router);
+
+
 const server=http.createServer(app);
 server.listen(app.get('port'),()=>{
-    console.log('localhost:%d',app.get('port'));
+    console.log("%d번 포트에서 작동중입니다!",app.get('port'));
 });
